2cef714c2f7a46b803113d11601016119f8d8b6e
boostrepublique <guiome.flandre+boostrepublique@gmail.com>
2015-01-17 23:32:04 +0100

    beta version

--- js/script.js
+++ js/script.js
@@ -1,28 +1,55 @@
+var STEPS = 5;
 var survey;
 
 jQuery(document).ready(function() {
-  survey = survey({ total: 4 });
+  survey = survey({ total: STEPS });
 
   init_listeners();
+  init_nav();
   animate_section(true);
 });
 
 var init_listeners = function() {
   jQuery('.next').on('click', function(e) {
     e.preventDefault();
+
+    if(survey.current() > 0 && survey.current() < STEPS - 1) {
+      jQuery('.section:visible .next').attr('disabled', 'disabled');
+    }
+
     survey.next();
     animate_section(false, function() {
+      if(survey.current() === 0) {
+        jQuery('#nav div').removeClass('selected');
+      }
+      else {
+        jQuery('#nav div:nth-child(' + survey.current() + ')').addClass('selected');
+      }
+
       jQuery('.section').hide();
+      jQuery('.answers .btn.selected').removeClass('selected');
+      jQuery(document).scrollTop(0);
       jQuery('#page-' + survey.current()).show();
-      animate_section(true);
+      animate_section(true, function() {
+        if(survey.current() === STEPS - 1) {
+          launch_confetti();
+        }
+      });
     });
   });
 
-  jQuery('.answers .btn').on('click', function() {
+  jQuery('.answers .btn').on('click', function(e) {
+    e.preventDefault();
     jQuery('.answers .btn.selected').removeClass('selected');
     jQuery(this).addClass('selected');
     jQuery('.section:visible .next').removeAttr('disabled');
   });
+};
+
+var init_nav = function() {
+  var width = jQuery('#nav').width() + 5;
+  var num_steps = jQuery('#nav div').length;
+  jQuery('#nav div').width((width/num_steps) - 5);
 };
 
 var animate_section = function(show, cb_) {
@@ -39,7 +66,7 @@ var animate_section = function(show, cb_) {
       setTimeout(function() {
         jQuery('.section:visible .logos img').animate({
           'opacity': .25
-        }, 2000);
+        }, 1000);
       }, 500);
 
       if(typeof cb_ === 'function') {
@@ -66,4 +93,9 @@ var animate_section = function(show, cb_) {
       'opacity': 0
     }, 500);
   }
+};
+
+var is_mobile = function() {
+  return /Mobi/.test(navigator.userAgent) &&
+         jQuery(window).height() <= 650;
 };
